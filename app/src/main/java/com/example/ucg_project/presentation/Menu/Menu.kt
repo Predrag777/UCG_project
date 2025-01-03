fb253cfc814cae062e38bb8eb90ab2e66b0aa1f8
Predrag777 <predragbrajovicSS@gmail.com>
2025-01-03 09:07:46 +0100

    Menu view

---
+++ app/src/main/java/com/example/ucg_project/presentation/Menu/Menu.kt
@@ -0,0 +1,14 @@
+package com.example.ucg_project.presentation.Menu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ucg_project.R
+
+class Menu:AppCompatActivity() {
+    override fun onCreate(saveInstanceState:Bundle?){
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.main_menu)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/ucg_project/presentation/Menu/MenuAdapterClass.kt
@@ -0,0 +1,35 @@
+package com.example.ucg_project.presentation.Menu
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ucg_project.R
+import com.example.ucg_project.data.MenuDataClass
+import com.example.ucg_project.presentation.news.AdapterClass
+import com.example.ucg_project.presentation.news.AdapterClass.ViewHolderClass
+
+class MenuAdapterClass (private val datalist:ArrayList<MenuDataClass>):  RecyclerView.Adapter<AdapterClass.ViewHolderClass>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AdapterClass.ViewHolderClass {
+        val itemView=
+            LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return com.example.ucg_project.presentation.news.AdapterClass.ViewHolderClass(itemView)
+    }
+
+    override fun onBindViewHolder(holder: AdapterClass.ViewHolderClass, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+    class ViewHolderClass(itemView: View): RecyclerView.ViewHolder(itemView){
+        val rvImage: ImageView =itemView.findViewById(R.id.image)
+    }
+}
